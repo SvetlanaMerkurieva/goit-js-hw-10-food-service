c846f2dcef3457d70938597666a14f36fd20cf41
SvetlanaMerkurieva <merkureva83@gmail.com>
2021-09-26 19:37:35 +0300

    Добавляет темную и светлую темы

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+const inputToggle = document.querySelector('#theme-switch-toggle');
+inputToggle.addEventListener("change", onChangeTheme);
+function onChangeTheme(evt) {
+    evt.target.checked ? document.body.classList.add("dark-theme") & localStorage.setItem("theme", "dark")
+        : document.body.classList.replace("dark-theme", "light-theme") & localStorage.setItem("theme", "light");
+}
+currentTheme();
+
+function currentTheme() {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "light") {
+        document.body.classList.add("light-theme");
+        inputToggle.checked = false;
+    } else {
+        document.body.classList.add("dark-theme");
+        inputToggle.checked = true;
+    }
+}
